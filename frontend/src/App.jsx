ca47de534008836f77d43a6fd429fb6d05a1a499
Quantoreq <kamil200123@gmail.com>
2025-02-13 22:33:20 +0100

    test commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        App for tracking habits
+      </div>
+    </>
+  )
+}
+
+export default App
